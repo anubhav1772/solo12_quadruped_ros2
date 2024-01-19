76ca17abce68a8383acb7ce464b1edb75c798220
anubhav1772 <anubhavsingh55182@gmail.com>
2024-01-20 00:06:32 +0530

    rviz; no gazebo

---
+++ solo12_gazebo/launch/view_solo12.launch.py
@@ -0,0 +1,81 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gazebo_package",
+            default_value="solo12_gazebo",
+            description="Description package with robot URDF/xacro files. Usually the argument \
+        is not set, it enables use of a custom description.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gui",
+            default_value="true",
+            description="Start Rviz2 and Joint State Publisher gui automatically \
+        with this launch file.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "prefix",
+            default_value='""',
+            description="Prefix of the joint names, useful for \
+        multi-robot setup. If changed than also joint names in the controllers' configuration \
+        have to be updated.",
+        )
+    )
+
+    # Initialize Arguments
+    gazebo_package = LaunchConfiguration("gazebo_package")
+    gui = LaunchConfiguration("gui")
+    prefix = LaunchConfiguration("prefix")
+
+    desc_pkg_share = FindPackageShare(package='solo12_description').find('solo12_description')
+    xacro_file_path = os.path.join(desc_pkg_share, 'urdf/', 'solo12_robot.urdf.xacro')
+    robot_description_config = xacro.process_file(xacro_file_path)
+    robot_desc = robot_description_config.toxml()
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(gazebo_package), "rviz", "solo12.rviz"]
+    )
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        condition=IfCondition(gui),
+    )
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[{"robot_description": robot_desc}],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        condition=IfCondition(gui),
+    )
+
+    nodes = [
+        joint_state_publisher_node,
+        robot_state_publisher_node,
+        rviz_node,
+    ]
+
+    return LaunchDescription(declared_arguments + nodes)
